a9f1f75170cb5602590ef850639e8fb67dd58cf3
Charlie <ceg98@drexel.edu>
2024-01-19 08:57:34 -0500

    cleaning repository

--- sample.py
+++
@@ -1,9 +0,0 @@
-{'Records': ['s3': {'s3SchemaVersion': '1.0', 'configurationId': '8e793ef0-1eea-4f9d-8b3d-b5410eb765f9', 'bucket': {'name': 'production-logs-tokenized-toast', 
-        'ownerIdentity': {'principalId': 'A3CTNZYENO40WA'}, 
-        'arn': 'arn:aws:s3:::production-logs-tokenized-toast'}, 
-        'object': {'key': 'ArticleExtraction/working-logs/1705384346.log', 
-                'size': 821664, 
-                'eTag': '6208902b1db65de29cfd90e4621f4195', 
-                'sequencer': '0065A6199AF105FCA5'}}]
-}
-    
